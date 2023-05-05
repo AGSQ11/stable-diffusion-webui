2a3a23b8b706458bb05a26718534d683c6fd38a5
AGSQ <67051233+AGSQ11@users.noreply.github.com>
2023-05-05 20:53:06 +0300

    Update webui.sh

--- webui.sh
+++ webui.sh
@@ -119,7 +119,7 @@ esac
 if echo "$gpu_info" | grep -q "AMD" && [[ -z "${TORCH_COMMAND}" ]]
 then
     # AMD users will still use torch 1.13 because 2.0 does not seem to work.
-    export TORCH_COMMAND="pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.4.2"
+    export TORCH_COMMAND="pip install torch==1.13.1+rocm5.2 torchvision==0.14.1+rocm5.2 --index-url https://download.pytorch.org/whl/rocm5.2"
 fi  
 
 for preq in "${GIT}" "${python_cmd}"
